33bff3feb633252aaa4555c5f90ca46fecb1a965
sg.object <sg.object@gmail.com>
2023-06-03 22:55:50 +0900

    first commit

---
+++ src/main/java/com/sg/oauth/SgOauthApplication.java
@@ -0,0 +1,13 @@
+package com.sg.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sg/oauth/SgOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
